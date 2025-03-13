50bbae8a052cb3e1551f4ec0f5b9699f012d7c51
vinu1123 <vinodini.n11@gmail.com>
2025-03-13 14:33:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-required_providers {
-aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
   region = "ap-southeast-2"
 }
