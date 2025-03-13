a64fa3635bf1749a063b5a408a45c5f5c0b31248
vinu1123 <vinodini.n11@gmail.com>
2025-03-13 15:27:01 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,13 +1,17 @@
 terraform {
-required_providers {
-aws = {
+  required_providers {
+    aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
   required_version = ">= 1.2.0"
-}
 
-provider "aws" {
-  region = "ap-southeast-2"
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-arjun12"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
 }
